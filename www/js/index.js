f15dd52f164d89ca6ad8148da756518652514a31
goparklubanic <ngoparios@yahoo.com>
2018-03-30 15:40:34 +0700

    change index view

--- www/js/index.js
+++ www/js/index.js
@@ -64,38 +64,43 @@ var app = {
 
   //Function that executes once registered with Firebase
     push.on('registration', function(data) {
-      console.log("registration event: " + data.registrationId);
-      console.log("device type: " + device.platform);
-      var info = data.registrationId+", "+device.platform;
-      document.getElementById('info').innerHTML = info;
-      document.write('<div>' + data.registrationId + '</div>');
+    //  console.log("registration event: " + data.registrationId);
+    //  console.log("device type: " + device.platform);
+    //  var info = data.registrationId+", "+device.platform;
+      document.getElementById('regId').innerHTML = data.registrationId;
+      document.getElementById('platform').innerHTML = device.platform;
+      // document.write('<div>' + data.registrationId + '</div>');
   });
 
   //Error handler
   push.on('error', function(e) {
-    console.log("push error: " + e.message);
-    document.getElementById('info').innerHTML = "error: "+e.message;
+  //  console.log("push error: " + e.message);
+    document.getElementById('status').innerHTML = "error: "+e.message;
   });
 
   //Function that executes when a notification is received
   push.on('notification', function(data) {
-    console.log('notification event received');
-    document.getElementById('info').innerHTML = 'notification event received';
+    // console.log('notification event received');
+    var pesan = data.message +'<br/>'+data.title;
+    document.getElementById('status').innerHTML = 'notification event received';
+    document.getElementById('pesan').innerHTML = pesan;
+    /*
     navigator.notification.alert(
     data.message, // message
     null, // callback
     data.title, // title
     'Ok' // buttonName
     );
+    */
   });
 
   //Push finish setup function
   push.finish(function() {
-    console.log('Push Setup Success');
-    document.getElementById('info').innerHTML = "Push success";
+    // console.log('Push Setup Success');
+    document.getElementById('status').innerHTML = "Push Success";
     }, function() {
-    console.log('Push Setup Error');
-    document.getElementById('info').innerHTML = "Push Failed";
+    // console.log('Push Setup Error');
+    document.getElementById('status').innerHTML = "Push Failed";
   });
 
   }
